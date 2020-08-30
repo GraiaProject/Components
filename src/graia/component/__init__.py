5026609a59bbadd7e55c463623e5b26ed96cc4b5
Elaina <31543961+GreyElaina@users.noreply.github.com>
2020-08-30 22:48:19 +0800

    Update __init__.py

--- src/graia/component/__init__.py
+++ src/graia/component/__init__.py
@@ -1,6 +1,5 @@
 from typing import Callable, Generic, Sequence, TypeVar, Union
-from graia.application.message.elements.internal import InternalElement
-from graia.application.message import ExternalElement
+from graia.application.message.elements import InternalElement, ExternalElement
 from graia.broadcast.entities.decorater import Decorater
 from graia.broadcast.interfaces.decorater import DecoraterInterface
 from graia.application.entry import MessageChain
